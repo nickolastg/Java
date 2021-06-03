7d0b631f193be571fcf0187229c7199288b92724
Nickolas Tavares Garcia <83840121+nickolastg@users.noreply.github.com>
2021-06-03 01:22:37 -0300

    Add files via upload

---
+++ TarefaS02_01/Main.java
@@ -0,0 +1,32 @@
+package TarefaS02_01;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+	  	Scanner sc = new Scanner(System.in);
+
+	    System.out.println("Digite o número de notas a serem inseridas");
+    int numeroNotas = sc.nextInt();
+
+    float[] notas = new float[numeroNotas];
+    //  System.out.println("Entre com nota 1:");
+    //  notas[0] = sc.nextFloat();
+    //  System.out.println("Entre com nota 2:");
+    //  notas[1] = sc.nextFloat();
+    //  System.out.println("Entre com nota 3:");
+    //  notas[2] = sc.nextFloat(Int);
+
+    for(int i = 0; i < numeroNotas; ++i){
+      System.out.println("Entre com a nota " +(i+1));
+      notas[i] = sc.nextFloat();
+    }
+    float soma = 0;
+    for (int i = 0; i < notas.length; ++i){
+      soma = soma + notas[i];
+    }
+
+    float media = soma/numeroNotas;
+    System.out.println("A Média é: " + media);
+	    sc.close();
+	  }
+}
