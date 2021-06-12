0d8d2b5e33c0d5af9ff3c859ffa1b5cb2d72a6f3
Nickolas Tavares Garcia <83840121+nickolastg@users.noreply.github.com>
2021-06-12 19:31:12 -0300

    Update Array02.java

--- array/Array02.java
+++ array/Array02.java
@@ -1,37 +1,36 @@
 package array;
 import java.util.Scanner;
+
 class Array02 {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    System.out.println("Entre com o número de elementos");
-    int numeroElementos = sc.nextInt();
+        System.out.println("Entre com o nÃºmero de elementos");
+        int numeroElementos = sc.nextInt();
 
-    int[] numeros = new int[numeroElementos];
+        int[] numeros = new int[numeroElementos];
 
-    for (int i = 0; i < numeroElementos; ++i){
-      System.out.println("Entre com o número " +(i+1));
-      numeros[i] = sc.nextInt();
+        for (int i = 0; i < numeroElementos; ++i) {
+            System.out.println("Entre com o nÃºmero " + (i + 1));
+            numeros[i] = sc.nextInt();
+        }
+
+        int maior = 0;
+        int menor = 0;
+
+        for (int i = 0; i < numeros.length; ++i) {
+            if (i == 0) {
+                maior = numeros[i];
+                menor = numeros[i];
+            } else if (numeros[i] > maior) {
+                maior = numeros[i];
+            } else if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+
+        System.out.println("Maior nÃºmero: " + maior);
+        System.out.println("Menor nÃºmero: " + menor);
+        sc.close();
     }
-
-    int maior = 0;
-    int menor = 0;
-
-    for (int i = 0; i < numeros.length; ++i){
-      if (i==0){
-        maior = numeros[i];
-        menor = numeros[i];
-      }
-      else if (numeros[i] > maior){
-        maior = numeros[i];
-      }
-      else if (numeros[i] < menor){
-        menor = numeros[i];
-      }
-    }
-
-    System.out.println("Maior número: " + maior);
-    System.out.println("Menor número: " + menor);
-    sc.close();
-  }
 }
