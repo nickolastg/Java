ea08dc17fbd268863afbc3b4ba0fb5415ab5d062
Nickolas Tavares Garcia <83840121+nickolastg@users.noreply.github.com>
2021-06-12 19:26:45 -0300

    Update TarefaS04_01.java

--- modularização/TarefaS04_01.java
+++ modularização/TarefaS04_01.java
@@ -1,35 +1,37 @@
-package modularização;
+package modularizaÃ§Ã£o;
 
 class TarefaS04_01 {
-	  public static void main(String[] args) {
-	    String nome = args[0];
+    public static void main(String[] args) {
+        String nome = args[0];
 
-	    double[] notas = new double[args.length-1];
+        double[] notas = new double[args.length - 1];
 
-	    if (notas.length >= 2){
-	      for (int i = 0; i < notas.length; ++i){
-	        notas[i] = Double.parseDouble(args[i+1]);
-	    }
-	      double media = Estatistica.calcularMedia(notas); 
-	      exibirNota(media, nome);
+        if (notas.length >= 2) {
+            for (int i = 0; i < notas.length; ++i) {
+                notas[i] = Double.parseDouble(args[i + 1]);
+            }
+            double media = Estatistica.calcularMedia(notas);
+            exibirNota(media, nome);
 
-	      double maior = Estatistica.calculoMaior(notas);
-	      exibirMaior(maior);
+            double maior = Estatistica.calculoMaior(notas);
+            exibirMaior(maior);
 
-	      double menor = Estatistica.calculoMenor(notas);
-	      exibirMenor(menor);
+            double menor = Estatistica.calculoMenor(notas);
+            exibirMenor(menor);
 
-	    }else{System.out.println("Você digitou menos de duas notas.");}
-	  }
-	  // "João da Silva tem média 9.5"
-	  public static void exibirNota(double nota, String nome){
-	    System.out.println("" + nome + " tem média: " + String.format("%.2f", nota));
-	  }
-	  public static void exibirMaior(double maiorNota){
-	    System.out.println("A maior nota do aluno é: " + String.format("%.2f", maiorNota));
-	  }
-	  public static void exibirMenor(double menorNota){
-	    System.out.println("A menor nota do aluno é: " + String.format("%.2f", menorNota));
-	  }
+        } else {
+            System.out.println("VocÃª digitou menos de duas notas.");
+        }
+    }
+    // "JoÃ£o da Silva tem mÃ©dia 9.5"
+    public static void exibirNota(double nota, String nome) {
+        System.out.println("" + nome + " tem mÃ©dia: " + String.format("%.2f", nota));
+    }
+    public static void exibirMaior(double maiorNota) {
+        System.out.println("A maior nota do aluno Ã©: " + String.format("%.2f", maiorNota));
+    }
+    public static void exibirMenor(double menorNota) {
+        System.out.println("A menor nota do aluno Ã©: " + String.format("%.2f", menorNota));
+    }
 
 }
