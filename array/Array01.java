ef550a7ed3e8825490d0020c00965cc1d771cc92
Nickolas Tavares Garcia <83840121+nickolastg@users.noreply.github.com>
2021-06-12 19:23:55 -0300

    Add files via upload

---
+++ array/Array01.java
@@ -0,0 +1,25 @@
+package array;
+import java.util.Scanner;
+
+public class Array01 {
+  public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Digite o número de notas a serem inseridas");
+    int numeroNotas = sc.nextInt();
+
+    float[] notas = new float[numeroNotas];
+    for(int i = 0; i < numeroNotas; ++i){
+      System.out.println("Entre com a nota " +(i+1));
+      notas[i] = sc.nextFloat();
+    }
+    float soma = 0;
+    for (int i = 0; i < notas.length; ++i){
+      soma = soma + notas[i];
+    }
+
+    float media = soma/numeroNotas;
+    System.out.println("A Média é: " + media);
+	    sc.close();
+  }
+}
