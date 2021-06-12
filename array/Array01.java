eadee9044cb64f30090a4d04a9c10037c0b20b97
Nickolas Tavares Garcia <83840121+nickolastg@users.noreply.github.com>
2021-06-12 19:29:53 -0300

    Update Array01.java

--- array/Array01.java
+++ array/Array01.java
@@ -2,24 +2,24 @@ package array;
 import java.util.Scanner;
 
 public class Array01 {
-  public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	System.out.println("Digite o número de notas a serem inseridas");
-    int numeroNotas = sc.nextInt();
+        System.out.println("Digite o nÃºmero de notas a serem inseridas");
+        int numeroNotas = sc.nextInt();
 
-    float[] notas = new float[numeroNotas];
-    for(int i = 0; i < numeroNotas; ++i){
-      System.out.println("Entre com a nota " +(i+1));
-      notas[i] = sc.nextFloat();
+        float[] notas = new float[numeroNotas];
+        for (int i = 0; i < numeroNotas; ++i) {
+            System.out.println("Entre com a nota " + (i + 1));
+            notas[i] = sc.nextFloat();
+        }
+        float soma = 0;
+        for (int i = 0; i < notas.length; ++i) {
+            soma = soma + notas[i];
+        }
+
+        float media = soma / numeroNotas;
+        System.out.println("A MÃ©dia Ã©: " + media);
+        sc.close();
     }
-    float soma = 0;
-    for (int i = 0; i < notas.length; ++i){
-      soma = soma + notas[i];
-    }
-
-    float media = soma/numeroNotas;
-    System.out.println("A Média é: " + media);
-	    sc.close();
-  }
 }
