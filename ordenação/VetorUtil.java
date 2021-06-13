9ad7a620f0febb66cbeaf965ef8ee82bdb0d6f53
Nickolas Tavares Garcia <83840121+nickolastg@users.noreply.github.com>
2021-06-13 11:50:52 -0300

    Update VetorUtil.java

--- ordenação/VetorUtil.java
+++ ordenação/VetorUtil.java
@@ -1,20 +1,20 @@
 package ordenação;
 
 public class VetorUtil {
-	  public static int[] copiar(int[] vetor) {
-		    int[] novoVetor = new int[vetor.length];
-		    for(int i = 0; i < novoVetor.length; i++) {
-		      novoVetor[i] = vetor[i];
-		    }
-		    
-		    return novoVetor;
-	  }
-		 
-	  public static void imprimirVetor(int[] vetor) {
-		  for(int i = 0; i < vetor.length; i++) {
-			System.out.println(vetor[i]);
-		  }
-		  System.out.println("------------------");
-	  }
+    public static int[] copiar(int[] vetor) {
+        int[] novoVetor = new int[vetor.length];
+        for (int i = 0; i < novoVetor.length; i++) {
+            novoVetor[i] = vetor[i];
+        }
+
+        return novoVetor;
+    }
+
+    public static void imprimirVetor(int[] vetor) {
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
+        System.out.println("------------------");
+    }
 
 }
